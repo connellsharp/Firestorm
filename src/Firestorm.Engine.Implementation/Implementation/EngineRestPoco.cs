3ed7c4b6663c6456a1969bce6fff6ef358f998a9
Connell <connellsharp@gmail.com>
2017-12-03 15:59:02 +0000

    Poco now just derives from scalar. Same code - less to test. It's temporary anyway?

--- src/Firestorm.Engine.Implementation/Implementation/EngineRestPoco.cs
+++ src/Firestorm.Engine.Implementation/Implementation/EngineRestPoco.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Firestorm.Engine.Fields;
 using JetBrains.Annotations;
 
 namespace Firestorm.Engine
@@ -10,46 +6,13 @@ namespace Firestorm.Engine
     /// Represents a non-engine item that has been returned as if it were a scalar value.
     /// </summary>
     /// <remarks>
-    /// TODO shouldn't actually be scalar, but this is just copied.
+    /// TODO shouldn't actually be scalar, but it is basically the same thing atm.
+    /// We want to add stuff in for naming convention switching and individual field selecting in querystring.
     /// </remarks>
-    public class EngineRestPoco<TItem> : IRestScalar 
+    public class EngineRestPoco<TItem> : EngineRestScalar<TItem>
         where TItem : class
     {
-        private readonly IEngineContext<TItem> _context;
-        private readonly IDeferredItem<TItem> _item;
-        private readonly INamedField<TItem> _field;
-
-        internal EngineRestPoco([NotNull] IEngineContext<TItem> context, [NotNull] IDeferredItem<TItem> item, [NotNull] INamedField<TItem> field)
-        {
-            if (context == null) throw new ArgumentNullException(nameof(context));
-            if (item == null) throw new ArgumentNullException(nameof(item));
-            if (field == null) throw new ArgumentNullException(nameof(field));
-
-            _context = context;
-            _item = item;
-            _field = field;
-        }
-
-        public async Task<object> GetAsync()
-        {
-            if (!_context.AuthorizationChecker.CanGetField(_item, _field))
-                throw new NotAuthorizedForFieldException(AuthorizableVerb.Get, _field.Name);
-
-            IFieldReader<TItem> value = _field.Reader;
-
-            return await ScalarFieldHelper.LoadScalarValueAsync(value, _item.Query, _context.Repository.ForEachAsync);
-        }
-
-        public async Task<Acknowledgment> EditAsync(object value)
-        {
-            if (!_context.AuthorizationChecker.CanEditField(_item, _field))
-                throw new NotAuthorizedForFieldException(AuthorizableVerb.Edit, _field.Name);
-
-            await _item.LoadAsync(); // TODO: is this necessary?
-            await _field.Writer.SetValueAsync(_item, value, _context.Transaction);
-            await _context.Transaction.SaveChangesAsync();
-
-            return new Acknowledgment();
-        }
+        public EngineRestPoco([NotNull] IEngineContext<TItem> context, [NotNull] IDeferredItem<TItem> item, [NotNull] INamedField<TItem> field) : base(context, item, field)
+        { }
     }
 }
