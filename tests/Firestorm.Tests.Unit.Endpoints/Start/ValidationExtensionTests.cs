4c97166256d87223362ebdcac759b4072544bde4
Connell <connellsharp@gmail.com>
2017-12-05 19:04:48 +0000

    Added some more tests

---
+++ tests/Firestorm.Tests.Unit.Endpoints/Start/ValidationExtensionTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Firestorm.Endpoints;
+using Firestorm.Endpoints.Start;
+using Firestorm.Tests.Unit.Endpoints.Stubs;
+using Xunit;
+
+namespace Firestorm.Tests.Unit.Endpoints.Start
+{
+    public class ValidationExtensionTests
+    {
+        [Fact]
+        public void EnsureValid_BasicConfigWithStartResourceFactory_DoesntThrow()
+        {
+            var config = new FirestormConfiguration
+            {
+                StartResourceFactory = new SingletonStartResourceFactory(null)
+            };
+
+            config.EnsureValid();
+        }
+
+        [Fact]
+        public void EnsureValid_BasicConfigWithNullStartResourceFactory_Throws()
+        {
+            var config = new FirestormConfiguration
+            {
+                StartResourceFactory = null
+            };
+
+            Action ensureValid = () => config.EnsureValid();
+
+            Assert.Throws<FirestormConfigurationException>(ensureValid);
+        }
+
+        [Fact]
+        public void EnsureValid_NullQueryStringConfiguration_Throws()
+        {
+            var config = new FirestormConfiguration
+            {
+                StartResourceFactory = new SingletonStartResourceFactory(null),
+                EndpointConfiguration = new RestEndpointConfiguration
+                {
+                    QueryStringConfiguration = null
+                }
+            };
+
+            Action ensureValid = () => config.EnsureValid();
+
+            Assert.Throws<FirestormConfigurationException>(ensureValid);
+        }
+
+        [Fact]
+        public void EnsureValid_NullRequestStrategies_Throws()
+        {
+            var config = new FirestormConfiguration
+            {
+                StartResourceFactory = new SingletonStartResourceFactory(null),
+                EndpointConfiguration = new RestEndpointConfiguration
+                {
+                    RequestStrategies = null
+                }
+            };
+
+            Action ensureValid = () => config.EnsureValid();
+
+            Assert.Throws<FirestormConfigurationException>(ensureValid);
+        }
+
+        [Fact]
+        public void EnsureValid_NullResponseContentGenerator_Throws()
+        {
+            var config = new FirestormConfiguration
+            {
+                StartResourceFactory = new SingletonStartResourceFactory(null),
+                EndpointConfiguration = new RestEndpointConfiguration
+                {
+                    ResponseContentGenerator = null
+                }
+            };
+
+            Action ensureValid = () => config.EnsureValid();
+
+            Assert.Throws<FirestormConfigurationException>(ensureValid);
+        }
+    }
+}
