63d1e895fcd6f7d0da313d778f66f87631609231
Connell <connellsharp@gmail.com>
2018-01-10 01:38:54 +0000

    Split file and made wrapper internal

--- src/Firestorm.Engine.Subs/Repositories/RepositoryEventWrapper.cs
+++ src/Firestorm.Engine.Subs/Repositories/RepositoryEventWrapper.cs
@@ -9,13 +9,13 @@ namespace Firestorm.Engine.Subs.Repositories
     /// <summary>
     /// Wraps and implements <see cref="IEngineRepository{TItem}"/> and calls methods on the given events object when repository methods are called.
     /// </summary>
-    public class RepositoryEventWrapper<TItem>: IEngineRepository<TItem>
+    internal class RepositoryEventWrapper<TItem>: IEngineRepository<TItem>
         where TItem : class
     {
         private readonly IEngineRepository<TItem> _repository;
         private readonly IRepositoryEvents<TItem> _events;
 
-        public RepositoryEventWrapper(IEngineRepository<TItem> repository, IRepositoryEvents<TItem> events)
+        internal RepositoryEventWrapper(IEngineRepository<TItem> repository, IRepositoryEvents<TItem> events)
         {
             _repository = repository;
             _events = events;
@@ -46,18 +46,6 @@ namespace Firestorm.Engine.Subs.Repositories
         public void MarkDeleted(TItem item)
         {
             _repository.MarkDeleted(item);
-        }
-    }
-
-    public static class RepositoryWrapperUtility
-    {
-        public static IEngineRepository<TItem> TryWrapEvents<TItem>(IEngineRepository<TItem> repository, IRepositoryEvents<TItem> events)
-            where TItem : class
-        {
-            if (events != null && events.HasAnyEvent)
-                return new RepositoryEventWrapper<TItem>(repository, events);
-
-            return repository;
         }
     }
 }
---
+++ src/Firestorm.Engine.Subs/Repositories/RepositoryWrapperUtility.cs
@@ -0,0 +1,17 @@
+﻿using Firestorm.Data;
+using Firestorm.Engine.Subs.Handlers;
+
+namespace Firestorm.Engine.Subs.Repositories
+{
+    public static class RepositoryWrapperUtility
+    {
+        public static IEngineRepository<TItem> TryWrapEvents<TItem>(IEngineRepository<TItem> repository, IRepositoryEvents<TItem> events)
+            where TItem : class
+        {
+            if (events != null && events.HasAnyEvent)
+                return new RepositoryEventWrapper<TItem>(repository, events);
+
+            return repository;
+        }
+    }
+}
