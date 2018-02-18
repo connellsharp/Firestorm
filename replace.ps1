ccc6d911ce5c466aa12fab579a0f94d8f5b4a123
Connell <connellsharp@gmail.com>
2018-02-18 00:00:03 +0000

    Deleted replace.ps1

--- replace.ps1
+++
@@ -1,7 +0,0 @@
-$configFiles = Get-ChildItem . *.csproj -rec
-foreach ($file in $configFiles)
-{
-    (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace "net452<", "net45<" } |
-    Set-Content $file.PSPath
-}
