19afb1bf45ee83142561f74c71b44769cfd26df5
Connell <connellsharp@gmail.com>
2018-03-12 02:13:04 +0000

    missed these files in last commit

---
+++ Reflectious/Reflectors/InstanceReflector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+
+namespace Firestorm
+{
+    public class InstanceReflector<TInstance> : InstanceReflector
+    {
+        private readonly TInstance _instance;
+
+        public InstanceReflector([NotNull] TInstance instance)
+            : base(instance)
+        {
+            _instance = instance;
+        }
+
+        internal InstanceReflector(Type type)
+            : base(type)
+        {
+        }
+
+        public MethodReflector<TInstance, TReturn> GetMethod<TReturn>(Expression<Func<TInstance, TReturn>> expression)
+        {
+            var finder = new ExpressionMethodFinder<TInstance, TReturn>(expression);
+            return new MethodReflector<TInstance, TReturn>(_instance, finder);
+        }
+
+        public PropertyReflector<TInstance, TProperty> GetProperty<TProperty>(Expression<Func<TInstance, TProperty>> propertyExpression)
+        {
+            var finder = new ExpressionPropertyFinder<TInstance, TProperty>(propertyExpression);
+            return new PropertyReflector<TInstance, TProperty>(_instance, finder);
+        }
+    }
+
+    public class InstanceReflector
+    {
+        private readonly object _instance;
+        protected readonly Type Type;
+
+        public InstanceReflector([NotNull] object instance)
+        {
+            _instance = instance ?? throw new ArgumentNullException(nameof(instance));
+            Type = _instance.GetType();
+        }
+
+        internal InstanceReflector(Type type)
+        {
+            Type = type;
+        }
+
+        public MethodReflector GetMethod(string methodName, Assume assume = Assume.Nothing)
+        {
+            var finder = FinderUtility.GetMethodFinder(Type, methodName, _instance, assume);
+            
+            return new MethodReflector(_instance, finder);
+        }
+
+        public PropertyReflector GetProperty(string propertyName)
+        {
+            var finder = new PropertyFinder(Type, propertyName, _instance == null);
+            return new PropertyReflector(_instance, finder);
+        }
+    }
+}
---
+++ Reflectious/Reflectors/StaticReflector.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Firestorm
+{
+    public class StaticReflector : StaticReflector<object>
+    {
+        internal StaticReflector(Type type)
+            : base(type)
+        {
+        }
+
+        [PublicAPI]
+        public new InstanceReflector WithInstance(object instance)
+        {
+            return new InstanceReflector(instance);
+        }
+
+        [PublicAPI]
+        public new MethodReflector GetConstructor()
+        {
+            return new MethodReflector(null, new ConstructorFinder(Type));
+        }
+    }
+
+    public class StaticReflector<TType> : InstanceReflector<TType>
+        where TType : class
+    {
+        internal StaticReflector(Type type)
+            : base(type)
+        {
+            Debug.Assert(typeof(TType).IsAssignableFrom(type));
+        }
+        
+        public StaticReflector()
+            : base(typeof(TType))
+        {
+        }
+
+        [PublicAPI]
+        public MethodReflector<TType, TType> GetConstructor()
+        {
+            return new MethodReflector<TType, TType>(null, new ConstructorFinder(Type));
+        }
+
+        [PublicAPI]
+        public TType CreateInstance(params object[] args)
+        {
+            return GetConstructor()
+                //.WithParameters(args.Select(a => a?.GetType())) // already happening in .Invoke
+                .Invoke(args);
+        }
+
+        [PublicAPI]
+        public InstanceReflector<TType> WithNewInstance()
+        {
+            return WithInstance(CreateInstance());
+        }
+
+        [PublicAPI]
+        public InstanceReflector<TType> WithInstance(TType instance)
+        {
+            return new InstanceReflector<TType>(instance);
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric(params Type[] types)
+        {
+            var genericType = Type.MakeGenericType(types);
+            return new StaticReflector(genericType);
+        }
+        
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1>()
+        {
+            return MakeGeneric(typeof(T1));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1, T2>()
+        {
+            return MakeGeneric(typeof(T1), typeof(T2));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1, T2, T3>()
+        {
+            return MakeGeneric(typeof(T1), typeof(T2), typeof(T3));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1, T2, T3, T4>()
+        {
+            return MakeGeneric(typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1, T2, T3, T4, T5>()
+        {
+            return MakeGeneric(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric<T1, T2, T3, T4, T5, T6>()
+        {
+            return MakeGeneric(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6));
+        }
+
+        [PublicAPI]
+        public StaticReflector MakeGeneric(IEnumerable<Type> types)
+        {
+            return MakeGeneric(types.ToArray());
+        }
+    }
+}
