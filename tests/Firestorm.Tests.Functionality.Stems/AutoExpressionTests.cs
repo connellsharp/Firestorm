be5ce6118c4dbcdd43188219b1e1effa6256d7d8
Connell <connellsharp@gmail.com>
2018-08-19 15:42:21 +0100

    Add AutoExpr attribute

---
+++ tests/Firestorm.Tests.Functionality.Stems/AutoExpressionTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Firestorm.Stems;
+using Firestorm.Stems.Attributes.Attributes;
+using Firestorm.Stems.Attributes.Basic.Attributes;
+using Firestorm.Tests.Functionality.Stems.Helpers;
+using Firestorm.Tests.Unit;
+using Firestorm.Tests.Unit.Engine.Implementation;
+using Xunit;
+
+namespace Firestorm.Tests.Functionality.Stems
+{
+    public class AutoExpressionTests
+    {
+        private readonly IRestCollection _restCollection;
+
+        public AutoExpressionTests()
+        {
+            var testContext = new StemTestContext();
+            _restCollection = testContext.GetArtistsCollection<ArtistsStem>();
+        }
+
+        public class ArtistsStem : Stem<Artist>
+        {
+            [Identifier]
+            [Get]
+            [AutoExpr]
+            public static int ID { get; }
+
+            [Get]
+            [AutoExpr]
+            public static string Name { get; set; }
+        }
+
+        [Fact]
+        public async Task Item_GetName_CorrectName()
+        {
+            var name = await _restCollection.GetItem("123").GetScalar("Name").GetAsync() as string;
+
+            Assert.Equal(TestRepositories.ArtistName, name);
+        }
+    }
+}
