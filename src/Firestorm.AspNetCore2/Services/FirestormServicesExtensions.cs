f0695e2913ed94a50b01b8bfcc15c71957be333a
Connell <connellsharp@gmail.com>
2018-11-15 21:05:26 +0000

    Moar cleanup

--- src/Firestorm.AspNetCore2/Services/FirestormServicesExtensions.cs
+++ src/Firestorm.AspNetCore2/Services/FirestormServicesExtensions.cs
@@ -1,17 +1,27 @@
-﻿using Firestorm.Host;
+﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Firestorm.AspNetCore2
-{
-    public static class FirestormServicesExtensions
-    {
-        /// <summary>
-        /// Adds Firestorm services.
-        /// </summary>
-        public static IFirestormServicesBuilder AddFirestorm(this IServiceCollection services)
-        {
-            IFirestormServicesBuilder builder = new AspNetCoreServicesBuilder(services);
-            return builder;
-        }
-    }
-}
+ {
+     public static class FirestormServicesExtensions
+     {
+         /// <summary>
+         /// Adds Firestorm services.
+         /// </summary>
+         public static IFirestormServicesBuilder AddFirestorm(this IServiceCollection services)
+         {
+             IFirestormServicesBuilder builder = new AspNetCoreServicesBuilder(services);
+             return builder;
+         }
+         
+         /// <summary>
+         /// Adds Firestorm services.
+         /// </summary>
+         public static IServiceCollection AddFirestorm(this IServiceCollection services, Action<IFirestormServicesBuilder> configureAction)
+         {
+             IFirestormServicesBuilder builder = new AspNetCoreServicesBuilder(services);
+             configureAction(builder);
+             return services;
+         }
+     }
+ }
