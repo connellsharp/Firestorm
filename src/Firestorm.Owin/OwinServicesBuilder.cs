4adf9020845dabd1353f110bfbef506f01d62c0f
Connell <connellsharp@gmail.com>
2018-11-18 19:08:09 +0000

    Using reflectious as a DI container (but it doesn't work yet)

--- src/Firestorm.Owin/OwinServicesBuilder.cs
+++ src/Firestorm.Owin/OwinServicesBuilder.cs
@@ -1,36 +1,74 @@
 ﻿using System;
-using Firestorm.Host;
+using System.Collections.Generic;
+using Reflectious;
 
 namespace Firestorm.Owin
 {
     public class OwinServicesBuilder : IFirestormServicesBuilder
     {
-        // TODO internal IOC container
-        
+        private readonly IDictionary<Type, Func<IFirestormServiceProvider, object>> _dictionary;
+
+        public OwinServicesBuilder()
+        {
+            _dictionary = new Dictionary<Type, Func<IFirestormServiceProvider, object>>();
+        }
+
         public IFirestormServicesBuilder Add<TService>(Func<IFirestormServiceProvider, TService> implementationFactory) 
             where TService : class
         {
-            throw new NotImplementedException();
+            _dictionary.Add(typeof(TService), implementationFactory);
+            return this;
         }
 
         public IFirestormServicesBuilder Add(Type serviceType)
         {
-            throw new NotImplementedException();
+            _dictionary.Add(serviceType, sp => CreateService(sp, serviceType));
+            return this;
         }
 
         public IFirestormServicesBuilder Add<TService>(TService implementationInstance) 
             where TService : class
         {
-            throw new NotImplementedException();
+            _dictionary.Add(typeof(TService), sp => implementationInstance);
+            return this;
         }
 
         public IFirestormServicesBuilder Add<TAbstraction, TImplementation>() 
             where TAbstraction : class where TImplementation : class, TAbstraction
         {
-            throw new NotImplementedException();
+            _dictionary.Add(typeof(TAbstraction), sp => CreateService(sp, typeof(TImplementation)));
+            return this;
         }
-        
+
+        private static object CreateService(IServiceProvider serviceProvider, Type serviceType)
+        {
+            return serviceType.Reflect()
+                .GetConstructor()
+                .FromServiceProvider(serviceProvider)
+                .Invoke();
+        }
+
         public IFirestormServiceProvider Build()
+        {
+            return new OwinServicesProvider(_dictionary);
+        }
+    }
+
+    public class OwinServicesProvider : IFirestormServiceProvider
+    {
+        private readonly IDictionary<Type, Func<IFirestormServiceProvider, object>> _dictionary;
+
+        public OwinServicesProvider(IDictionary<Type,Func<IFirestormServiceProvider,object>> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _dictionary[serviceType].Invoke(this);
+        }
+
+        public IServiceProvider GetRequestServiceProvider()
         {
             throw new NotImplementedException();
         }
