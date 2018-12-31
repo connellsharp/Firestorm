a0bc1f2fdb27974c517582c020ea461649313bd1
Connell <connellsharp@gmail.com>
2018-12-31 18:09:20 +0000

    Removed unused config method

--- tests/Firestorm.Tests.Examples.Music/Web/ExampleConfiguration.cs
+++ tests/Firestorm.Tests.Examples.Music/Web/ExampleConfiguration.cs
@@ -1,40 +1,16 @@
-﻿using Firestorm.Endpoints.Naming;
+﻿using Firestorm.Endpoints;
+using Firestorm.Endpoints.Naming;
 using Firestorm.Endpoints.Responses;
 using Firestorm.Endpoints.Web;
-using Firestorm.EntityFramework6;
-using Firestorm.Stems.AutoMap;
-using Firestorm.Stems.Roots;
-using Firestorm.Stems.Roots.DataSource;
-using Firestorm.Tests.Examples.Music.Data;
 
 namespace Firestorm.Tests.Examples.Music.Web
 {
     internal static class ExampleConfiguration
     {
         /// <summary>
-        /// Gets the example configuration using the public Stem types nested in the given <see cref="TTest"/> type.
+        /// Gets the example endpoint configuration.
         /// </summary>
-        public static FirestormConfiguration GetFirestormConfig<TTest>()
-        {
-            return new FirestormConfiguration
-            {
-                EndpointConfiguration = EndpointConfiguration,
-                StartResourceFactory = new StemsStartResourceFactory
-                {
-                    RootResourceFactory = new DataSourceRootResourceFactory
-                    {
-                        StemTypeGetter = new NestedTypeGetter(typeof(TTest)),
-                        DataSource = new EntitiesDataSource<ExampleDataContext>(),
-                    },
-                    StemConfiguration = new DefaultStemConfiguration
-                    {
-                        AutoPropertyMapper = new DefaultPropertyAutoMapper()
-                    }
-                }
-            };
-        }
-
-        public static DefaultRestEndpointConfiguration EndpointConfiguration
+        public static RestEndpointConfiguration EndpointConfiguration
         {
             get
             {
