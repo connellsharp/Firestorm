b19faa481fa9692197525c350a0bc3450c292a0a
Connell <connellsharp@gmail.com>
2019-01-11 01:27:47 +0000

    Added cors tests

---
+++ tests/Firestorm.AspNetCore2.IntegrationTests/CorsStartup.cs
@@ -0,0 +1,41 @@
+﻿using Firestorm.Endpoints;
+using Firestorm.Host;
+using Firestorm.Testing.Http;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using ServiceProviderServiceExtensions = Firestorm.Host.ServiceProviderServiceExtensions;
+
+namespace Firestorm.AspNetCore2.IntegrationTests
+{
+    public class CorsStartup
+    {
+        [UsedImplicitly]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            
+            services.Configure<DefaultRestEndpointConfiguration>(config =>
+            {
+                config.ResponseConfiguration.ShowDeveloperErrors = true;
+                //
+            });
+            
+            services.AddFirestorm()
+                .AddEndpoints(sp => ServiceProviderServiceExtensions.GetService<IOptions<DefaultRestEndpointConfiguration>>(sp).Value)
+                .AddStartResourceFactory(new IntegratedStartResourceFactory());
+        }
+
+        [UsedImplicitly]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(
+                options => options.AllowAnyOrigin().AllowAnyMethod()
+            );
+            
+            app.UseFirestorm();
+        }
+    }
+}
---
+++ tests/Firestorm.AspNetCore2.IntegrationTests/CorsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Firestorm.Testing.Http;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace Firestorm.AspNetCore2.IntegrationTests
+{
+    [UsedImplicitly]
+    public class CorsTests: HttpIntegrationTestsBase
+    {
+        public CorsTests()
+        : base(new KestrelIntegrationSuite<CorsStartup>(2225))
+        {
+        }
+
+        [Fact]
+        public async Task CorsHeaders_Send_CorsResponse()
+        {
+            var response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, "/")
+            {
+                Headers =
+                {
+                    { "Access-Control-Request-Headers", "content-type" },
+                    { "Access-Control-Request-Method", "GET" },
+                    { "Origin", "https://example.com" }
+                }
+            });
+            
+            ResponseAssert.Success(response);
+        }
+    }
+}
