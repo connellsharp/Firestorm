87428235550316fae07eb7a84e6cc509c9430534
Connell <connellsharp@gmail.com>
2019-01-13 23:28:05 +0000

    DoubleClient tests sharing port now too

---
+++ tests/Firestorm.Client.IntegrationTests/DoubleClientFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Firestorm.Testing.Http;
+
+namespace Firestorm.Client.IntegrationTests
+{
+    public class DoubleClientFixture : IDisposable
+    {
+        public DoubleClientFixture()
+        {
+            IntegrationSuite = new NetCoreIntegrationSuite<Startup>(Startup.Port);
+            IntegrationSuite.Start();
+        }
+        public IHttpIntegrationSuite IntegrationSuite { get; }
+
+        public void Dispose()
+        {
+            IntegrationSuite?.Dispose();
+        }
+    }
+}
