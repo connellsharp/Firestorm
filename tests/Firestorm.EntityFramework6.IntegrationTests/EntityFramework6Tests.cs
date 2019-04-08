a92041d91a8ab2c502b2b394ad5600e0662b850d
Connell <connellsharp@gmail.com>
2019-04-08 19:19:48 +0100

    Maybe fixed EF integration tests

--- tests/Firestorm.EntityFramework6.IntegrationTests/EntityFramework6Tests.cs
+++ tests/Firestorm.EntityFramework6.IntegrationTests/EntityFramework6Tests.cs
@@ -6,10 +6,12 @@ using Xunit;
 namespace Firestorm.EntityFramework6.IntegrationTests
 {
     [UsedImplicitly]
-    public class EntityFramework6Tests : BasicDataTests, IClassFixture<EntitiesDataTransaction<ExampleDataContext>>
+    public class EntityFramework6Tests : BasicDataTests, IClassFixture<ExampleFixture>
     {
-        public EntityFramework6Tests(EntitiesDataTransaction<ExampleDataContext> transaction) 
-            : base(transaction, new EntitiesRepository<Artist>(transaction.DbContext.Artists))
+        public EntityFramework6Tests(ExampleFixture fixture) 
+            : base(
+                new EntitiesDataTransaction<ExampleDataContext>(fixture.Context),
+                new EntitiesRepository<Artist>(fixture.Context.Artists))
         {
         }
     }
